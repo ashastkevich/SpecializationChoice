6f9b3b763bd0c517bb88693d0ccacaa1b89ee53f
Aleksei <ais_80@inbox.ru>
2023-01-29 10:27:01 +0300

    Task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(ref string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Console.ReadLine();
+}
+void ShrinkArray(ref string[] array)
+{
+    array = array.Where(val => val.Length <= 3).ToArray();
+}
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int strings_count = int.Parse(Console.ReadLine());
+string[] array = new string[strings_count];
+Console.WriteLine("Введите строки: ");
+FillArray(ref array);
+ShrinkArray(ref array);
+Console.WriteLine("Результат: ");
+for (int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i]);
